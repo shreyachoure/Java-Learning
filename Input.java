61b12617a3d4173e859807db5a3a7e02147ec498
Shreya Choure <56231644+shreyachoure@users.noreply.github.com>
2020-10-27 23:53:56 +0530

    To take input from user

---
+++ Input.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+class Input{
+public static void main(String[] args){
+Scanner input = new Scanner(System.in);
+System.out.print("Enter a number: ");
+int number = input.nextInt();
+System.out.println("The number is: "+number);
+}}
